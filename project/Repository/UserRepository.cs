82255e73ee196dbc75ff2339d2930a6fdbe56bc9
Kerneels Bezuidenhout <kerneels.bez@gmail.com>
2021-12-10 09:27:55 +0200

    Inject missing data provider

--- project/Repository/UserRepository.cs
+++ project/Repository/UserRepository.cs
@@ -1,4 +1,10 @@
 public class UserRepository {
+  private readonly IDataProvider _dataProvider;
+
+  public UserRepository(IDataProvider dataProvider) {
+    _dataProvider = dataProvider;
+  }
+
   public User GetByImmutableId(string immutableId) {
     var cmd = _dataProvider.CreateStoredProcedureCommand("[User].[UserGetByImmutableId]");
     cmd.AddInParameter("@ImmutableId", DbType.String, immutableId);
